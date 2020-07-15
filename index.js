61cadd1ac719f3b362f906c589535cba509d2332
Liam Tinney <ltinney@powerhrg.com>
2020-07-15 15:29:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 62
+const num2 = 1
 
-// Write your code here
+const multiply = num1 * num2
+
+const random = Math.random()
+
+const mod = 24 % 5
+
+const max = Math.max(1, 2, 3, 4, 20);
